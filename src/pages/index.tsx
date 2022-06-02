5edb449276f4cd8e697755eea991da58423b737d
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-06-02 16:03:21 -0300

    index do post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,15 @@ import { GetStaticProps } from 'next';
 import Link from 'next/link';
 import Header from '../components/Header';
 import { FiCalendar, FiUser } from 'react-icons/fi'
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/esm/locale/pt-BR/index.js';
 
 interface Post {
   uid?: string;
@@ -27,7 +31,23 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+  const [posts, setPosts] = useState<Post[]>(
+   
+  );
+
   return (
     <>
       <main className={commonStyles.container}>
@@ -59,9 +79,34 @@ export default function Home() {
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType(Prismic.Predicates.at('document.type', 'posts'),
+  {
+    pageSize: 1,
+  });
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_ublication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        substitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+
+    next_page: postsResponse.next_page,
+    results: posts, 
+  }
+
+  return {
+    props: {
+      postsPagination,
+    }
+  }
+};
